276c852e1c28ed9da543b3118edb89eb286da6e7
markusremplbauer <markus.remplbauer@gmx.at>
2022-01-25 13:18:44 +0100

    add Luggage and LuggageRepo

---
+++ src/main/java/at/htl/workloads/luggage/LuggageRepository.java
@@ -0,0 +1,39 @@
+package at.htl.workloads.luggage;
+
+import at.htl.workloads.luggage.Luggage;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@ApplicationScoped
+public class LuggageRepository {
+    public final EntityManager em;
+
+    public LuggageRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void add(Luggage luggage) {
+        this.em.persist(luggage);
+    }
+
+    public void update(Luggage luggage) {
+        this.em.merge(luggage);
+    }
+
+    public Luggage getLuggage(Long id) {
+        var query = this.em
+                .createQuery("select l from Luggage l where p.id = :id", Luggage.class)
+                .setParameter("id", id);
+
+        return query.getResultList().stream()
+                .findFirst().orElse(null);
+    }
+
+    public List<Luggage> getAll() {
+        return this.em
+                .createQuery("select l from Luggage l", Luggage.class)
+                .getResultList();
+    }
+}
