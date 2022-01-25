e1298d84736ad9b867dba7d12b7e4ab606754504
markusremplbauer <markus.remplbauer@gmx.at>
2022-01-25 13:10:18 +0100

    add Ticket and TicketRepo

---
+++ src/main/java/at/htl/workloads/ticket/Ticket.java
@@ -0,0 +1,18 @@
+package at.htl.workloads.ticket;
+
+import javax.persistence.*;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
