d09e250033b5d50e59830cc7fb87e8d2c97947fa
markusremplbauer <markus.remplbauer@gmx.at>
2022-01-25 13:14:33 +0100

    add Flight and FlightRepo

---
+++ src/main/java/at/htl/workloads/flight/Flight.java
@@ -0,0 +1,18 @@
+package at.htl.workloads.flight;
+
+import javax.persistence.*;
+
+@Entity
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
