24f052242736f04b427fd6b577ed74227b2f0d4a
markusremplbauer <markus.remplbauer@gmx.at>
2022-01-28 08:58:13 +0100

    add comments

--- backend/src/main/java/at/htl/InitBean.java
+++ backend/src/main/java/at/htl/InitBean.java
@@ -2,7 +2,6 @@ package at.htl;
 
 import at.htl.workloads.location.Location;
 import at.htl.workloads.person.Person;
-import at.htl.workloads.pilot.Pilot;
 import io.quarkus.runtime.StartupEvent;
 
 import java.time.LocalDate;
@@ -13,6 +12,7 @@ import javax.enterprise.event.Observes;
 @ApplicationScoped
 public class InitBean {
     void init(@Observes StartupEvent event){
+        // customers
         Person p1 = new Person("John", "Doe", "john.doe@gmail.com", "123456789", LocalDate.parse("19.12.1990"), "123456789");
         Person p2 = new Person("Jim", "Paul", "jim.pau@gmail.com", "987654321", LocalDate.parse("19.08.1990"), "987654321");
         Person p3 = new Person("Jane", "Doe", "jane.doe@gmail.com", "123456789", LocalDate.parse("19.09.1990"), "123456789");
@@ -20,9 +20,20 @@ public class InitBean {
         Person p5 = new Person("Jill", "Doe", "jill.doe@gmail.com", "123456789", LocalDate.parse("19.11.1990"), "123456789");
         Person p6 = new Person("Jack", "Doe", "jack.doe@gmail.com", "123456789", LocalDate.parse("19.01.1990"), "123456789");
 
+        // airports
         Location l1 = new Location("Austria", "Vienna", "Vienna International Airport", "1300 Schwechat", "VIE");
         Location l2 = new Location("Austria", "Graz", "Graz Airport", "8073 Feldkirchen bei Graz", "GRZ");
         Location l3 = new Location("Austria", "Linz", "Linz Airport", "Flughafenstraße 1, 4063 Hörsching", "LNZ");
-//        Pilot pilot = new Pilot()
+
+        // pilots
+
+        // luggage
+
+        // airplane
+
+        // flight
+
+        // ticket
+
     }
 }
