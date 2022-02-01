1cd3351579ce1e7269b237088b737dc7ee1a1027
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-01 13:56:10 +0100

    extend LocationRepository

--- backend/src/main/java/at/htl/workloads/location/LocationRepository.java
+++ backend/src/main/java/at/htl/workloads/location/LocationRepository.java
@@ -1,4 +1,35 @@
 package at.htl.workloads.location;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class LocationRepository {
+    private final EntityManager em;
+
+    public LocationRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void add(Location location){
+        this.em.persist(location);
+    }
+
+    public void update(Location location){
+        this.em.merge(location);
+    }
+
+    public Location getLocation(Long id) {
+        var query = this.em
+                .createQuery("select l from Location l where l.id = :id", Location.class)
+                .setParameter("id", id);
+
+        return query.getResultList().stream()
+                .findFirst().orElse(null);
+    }
+
+    public List<Location> getAll() {
+        return this.em
+                .createQuery("select l from Location l", Location.class)
+                .getResultList();
+    }
 }
