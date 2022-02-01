6a2665e614903d8db18996126edb5099101d5c1e
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-01 13:52:17 +0100

    extend TravelclassRepository

--- backend/src/main/java/at/htl/workloads/travelclass/TravelclassRepository.java
+++ backend/src/main/java/at/htl/workloads/travelclass/TravelclassRepository.java
@@ -1,4 +1,35 @@
 package at.htl.workloads.travelclass;
 
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class TravelclassRepository {
+    private final EntityManager em;
+
+    public TravelclassRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void add(Travelclass travelclass){
+        this.em.persist(travelclass);
+    }
+
+    public void update(Travelclass travelclass){
+        this.em.merge(travelclass);
+    }
+
+    public Travelclass getTravelclass(Long id) {
+        var query = this.em
+                .createQuery("select tr from Travelclass tr where tr.id = :id", Travelclass.class)
+                .setParameter("id", id);
+
+        return query.getResultList().stream()
+                .findFirst().orElse(null);
+    }
+
+    public List<Travelclass> getAll() {
+        return this.em
+                .createQuery("select tr from Travelclass tr", Travelclass.class)
+                .getResultList();
+    }
 }
