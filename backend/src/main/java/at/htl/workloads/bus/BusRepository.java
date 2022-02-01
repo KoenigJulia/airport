762dd1f14620867b029a2482a7a6acc78bb2f0d1
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-01 13:54:17 +0100

    extend BusRepository

--- backend/src/main/java/at/htl/workloads/bus/BusRepository.java
+++ backend/src/main/java/at/htl/workloads/bus/BusRepository.java
@@ -1,4 +1,37 @@
 package at.htl.workloads.bus;
 
+import at.htl.workloads.person.Person;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class BusRepository {
+    private final EntityManager em;
+
+    public BusRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    public void add(Bus bus){
+        this.em.persist(bus);
+    }
+
+    public void update(Bus bus){
+        this.em.merge(bus);
+    }
+
+    public Bus getBus(Long id) {
+        var query = this.em
+                .createQuery("select b from Bus b where b.id = :id", Bus.class)
+                .setParameter("id", id);
+
+        return query.getResultList().stream()
+                .findFirst().orElse(null);
+    }
+
+    public List<Bus> getAll() {
+        return this.em
+                .createQuery("select b from Bus b", Bus.class)
+                .getResultList();
+    }
 }
