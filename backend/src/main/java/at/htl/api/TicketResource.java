21d561ace1e9fe18f1a64e31555e9e400e99a346
KoenigJulia <julia04school@gmail.com>
2022-02-03 12:49:36 +0100

    ticketresource

---
+++ backend/src/main/java/at/htl/api/TicketResource.java
@@ -0,0 +1,36 @@
+package at.htl.api;
+
+import at.htl.workloads.person.Person;
+import at.htl.workloads.person.PersonRepository;
+import at.htl.workloads.ticket.Ticket;
+import at.htl.workloads.ticket.TicketRepository;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+public class TicketResource {
+    private final TicketRepository ticketRepository;
+
+    public TicketResource(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    @GET
+    @Path("all")
+    public Response getAllTickets() {
+        var allTickets = this.ticketRepository.getAll();
+        return Response.ok(allTickets).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response get(@PathParam("id") Long id) {
+        Ticket ticket = this.ticketRepository.getTicket(id);
+        return (ticket == null
+                ? Response.status(404)
+                : Response.ok(ticket))
+                .build();
+    }
+}
