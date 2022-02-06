64cc9d765a19d838ac8d530ffbcb199f0eb7cecd
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-06 14:46:31 +0100

    add field to Ticket class and modify TicketRepository

--- backend/src/main/java/at/htl/workloads/ticket/Ticket.java
+++ backend/src/main/java/at/htl/workloads/ticket/Ticket.java
@@ -1,11 +1,12 @@
 package at.htl.workloads.ticket;
 
 import at.htl.workloads.flight.Flight;
+import at.htl.workloads.luggage.Luggage;
 import at.htl.workloads.person.Person;
-import at.htl.workloads.seat.Seat;
 import at.htl.workloads.travelclass.TravelClass;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Ticket {
@@ -17,20 +18,25 @@ public class Ticket {
     @ManyToOne
     private Person person;
     private Double price;
-    @ManyToOne
-    private Seat seat;
+    private Integer seatNumber;
+    private String rowNumber;
     @ManyToOne
     private TravelClass travelclass;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ticket")
+    private List<Luggage> lugagge;
 
     //region constructor
     public Ticket() {
     }
-    public Ticket(Flight flight, Person person, Double price, Seat seat, TravelClass travelclass) {
+    public Ticket(Flight flight, Person person, Double price, Integer seatNumber, String rowNumber, TravelClass travelclass,
+                  List<Luggage> luggage) {
         this.flight = flight;
         this.person = person;
         this.price = price;
-        this.seat = seat;
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
         this.travelclass = travelclass;
+        this.lugagge = luggage;
     }
     //endregion
     //region gettersetter
@@ -52,5 +58,35 @@ public class Ticket {
     public void setPerson(Person person) {
         this.person = person;
     }
-    //endregion
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public Integer getSeatNumber() {
+        return seatNumber;
+    }
+    public void setSeatNumber(Integer seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+    public String getRowNumber() {
+        return rowNumber;
+    }
+    public void setRowNumber(String rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+    public TravelClass getTravelclass() {
+        return travelclass;
+    }
+    public void setTravelclass(TravelClass travelclass) {
+        this.travelclass = travelclass;
+    }
+    public List<Luggage> getLugagge() {
+        return lugagge;
+    }
+    public void setLugagge(List<Luggage> lugagge) {
+        this.lugagge = lugagge;
+    }
+//endregion
 }
