d1957778253c55260a150ea8d2ac20fef9dc1525
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-02-08 16:29:58 +0100

    Get flight by id on page load

--- frontend/src/app/modules/book/pages/book-flight/book-flight.component.ts
+++ frontend/src/app/modules/book/pages/book-flight/book-flight.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {BackendApiService} from "../../../../core/services/backend-api.service";
+import {Flight} from "../../../../shared/models/flight.model";
 
 @Component({
   selector: 'app-book-flight',
   templateUrl: './book-flight.component.html',
 })
 export class BookFlightComponent implements OnInit {
+  flight: Flight | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private backendApiService: BackendApiService) { }
 
   ngOnInit(): void {
+    this.backendApiService.getFlightById(Number.parseInt(<string>this.route.snapshot.paramMap.get("id"))).subscribe(res => {
+      this.flight = res;
+    });
   }
 
 }
