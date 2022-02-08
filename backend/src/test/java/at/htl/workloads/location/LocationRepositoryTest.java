e25301ea08d6a51297e4a5a227a5108efbcfff40
MichaelTran6624 <michael.tran6624@gmail.com>
2022-02-08 12:41:58 +0100

    create empty Repository-Tests

---
+++ backend/src/test/java/at/htl/workloads/location/LocationRepositoryTest.java
@@ -0,0 +1,7 @@
+package at.htl.workloads.location;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationRepositoryTest {
+
+}
---
+++ backend/src/test/java/at/htl/workloads/pilot/PilotRepositoryTest.java
@@ -0,0 +1,7 @@
+package at.htl.workloads.pilot;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PilotRepositoryTest {
+
+}
